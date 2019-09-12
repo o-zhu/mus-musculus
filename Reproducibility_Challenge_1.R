8b547b790e50cf83ea8903dc840351f11cd10ec9
Yihao Lu <yihaolu@uchicago.edu>
2019-09-12 13:45:03 -0400

    Question 1 Code

---
+++ Reproducibility_Challenge_1.R
@@ -0,0 +1,10 @@
+# Make sure the directory has data.
+bug_data <- read_csv("./data/cole_arthropod_data_1946.csv")
+n = nrow(bug_data)
+# Question 1
+total_number = sum(bug_data$C_count_of_boards_with_k_spiders)
+mean = mean(bug_data$C_count_of_boards_with_k_spiders)
+ggplot() + 
+  geom_point(aes(x = 1:n, y = (bug_data$C_count_of_boards_with_k_spiders)/total_number)) + 
+  geom_line(aes(x = 1:n, y = (dpois(0:17,1/mean))), color = "red") + 
+  scale_y_continuous(sec.axis = sec_axis(~.*total_number, name = "spider_data"))
