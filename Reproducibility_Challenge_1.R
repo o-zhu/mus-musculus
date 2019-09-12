4f2416da9ab54dca50213bb783b7ca75adeb2cbc
Lissa <erennert@uchicago.edu>
2019-09-12 15:59:56 -0500

    Added LGP fit and adjusted axis labels. Plot needs legend

--- Reproducibility_Challenge_1.R
+++ Reproducibility_Challenge_1.R
@@ -4,10 +4,13 @@ n = nrow(bug_data)
 
 library(ggplot2)
 
-# Question 1
+# Question 1 and 4
 total_number = sum(bug_data$C_count_of_boards_with_k_spiders)
 mean = mean(bug_data$C_count_of_boards_with_k_spiders)
 ggplot() + 
-  geom_point(aes(x = 1:n, y = (bug_data$C_count_of_boards_with_k_spiders)/total_number)) + 
+  geom_point(aes(x = 1:n, y = (bug_data$C_count_of_boards_with_k_spiders)/total_number))+
+  xlab("# of spiders") + ylab("Probability") + 
   geom_line(aes(x = 1:n, y = (dpois(0:17,1/mean))), color = "red") + 
-  scale_y_continuous(sec.axis = sec_axis(~.*total_number, name = "spider_data"))
+  geom_line(aes(x = 1:n, y = (dLGP(0:17,1/mean, 0))), color = "blue", linetype="dashed") +
+  scale_y_continuous(sec.axis = sec_axis(~.*total_number))
+
