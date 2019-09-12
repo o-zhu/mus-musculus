75fe271a20d45fdfc91273e282ecae5c16f97c90
CC <caichufan@uchicago.edu>
2019-09-12 19:50:12 -0400

    Chufan Q6

--- Reproducibility_Challenge_3.R
+++ Reproducibility_Challenge_3.R
@@ -33,3 +33,39 @@ weevil_plot <- weevil_plot + geom_line(data=weevil_data, aes(x=weevil_data$k_num
   geom_point(data=weevil_data, aes(x=weevil_data$k_number_of_eggs, y=weevil_data$weevil_poisson),  
              colour='#006400', shape=0, size = 3)
 weevil_plot
+
+
+#Question 6
+library(RMKdiscrete)
+library(ggplot2)
+
+weevil_data <- read.csv("mitchell_weevil_egg_data_1975.csv")
+total_count <- sum(weevil_data$C_count_of_beans_with_k_eggs)
+#the probability of laying k number of eggs
+weevil_data$p_weevil <- weevil_data$C_count_of_beans_with_k_eggs/total_count
+
+avg <- mean(weevil_data$C_count_of_beans_with_k_eggs)
+λ2<-
+  
+  #get the y axis for the standard Possion distribution with λ=1/avg
+  weevil_data$weevil_poisson <- dpois(0:4, 1/avg)
+weevil_data$weevil_LGP <- dLGP(0:4, 1, 1/avg*(1-λ2))
+
+weevil_rows <- nrow(weevil_data)
+
+#draw the real data point graph
+weevil_plot <- ggplot(data = weevil_data, aes(x = weevil_data$k_number_of_eggs, y = weevil_data$p_weevil)) + 
+  geom_point(size = 3) + xlab("# of eggs laid at the same area") + ylab("Probability")
+
+#draw the Poisson distribution curve
+weevil_plot <- weevil_plot + 
+  geom_line(data=weevil_data, aes(x=weevil_data$k_number_of_eggs, y=weevil_data$weevil_poisson), linetype='dashed', colour='#006400') +  
+  geom_point(data=weevil_data, aes(x=weevil_data$k_number_of_eggs, y=weevil_data$weevil_poisson), colour='#006400', shape=0, size = 3) +
+  scale_y_continuous(sec.axis = sec_axis(~.*total_number, name = "weevil_data")) 
+
+#draw the LGP distribution curve
+
+weevil_plot <- weevil_plot +
+  geom_line(data=weevil_data, aes(x=weevil_data$k_number_of_eggs, y=weevil_data$weevil_LGP), linetype='dashed', colour='005400') +  
+  geom_point(data=weevil_data, aes(x=weevil_data$k_number_of_eggs, y=weevil_data$weevil_LGP), colour='005400', shape=0, size = 3)
+weevil_plot
